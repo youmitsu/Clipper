7ae3ecb46643c981d2ea0eb6fdcf6f382bad464b
youmitsu <ymbullseye@gmail.com>
2019-01-03 18:02:01 +0900

    initializie ClippableView

---
+++ src/main/java/youmeee/co/jp/clippablelayout/ClippableView.kt
@@ -0,0 +1,13 @@
+package youmeee.co.jp.clippablelayout
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ClippableView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyle: Int = 0
+) : View(context, attributeSet, defStyle) {
+    
+}
