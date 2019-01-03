7e3797548e4c09f91209d3bb4b63edc59cfd248b
youmitsu <ymbullseye@gmail.com>
2019-01-03 15:55:28 +0900

    initial_commit

---
+++ src/main/java/youmeee/co/jp/clippablelayout/ClippableLayout.kt
@@ -0,0 +1,11 @@
+package youmeee.co.jp.clippablelayout
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class ClippableLayout @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyle: Int = 0
+) : FrameLayout(context, attributeSet, defStyle)
