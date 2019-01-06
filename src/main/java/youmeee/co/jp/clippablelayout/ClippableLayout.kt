00bcfe9972bbd2b04a3620b346ef28bf21e19b2f
youmitsu <ymbullseye@gmail.com>
2019-01-06 17:13:44 +0900

    とりあえずcommit

--- src/main/java/youmeee/co/jp/clippablelayout/ClippableLayout.kt
+++ src/main/java/youmeee/co/jp/clippablelayout/ClippableLayout.kt
@@ -2,10 +2,20 @@ package youmeee.co.jp.clippablelayout
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.FrameLayout
 
 class ClippableLayout @JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null,
     defStyle: Int = 0
-) : FrameLayout(context, attributeSet, defStyle)
+) : FrameLayout(context, attributeSet, defStyle) {
+
+    private val clippableView: ClippableView = ClippableView(context, attributeSet, defStyle)
+
+    init {
+        val view = View(context, attributeSet, defStyle)
+        view.setBackgroundColor(resources.getColor(R.color.error_color_material_dark))
+        addView(view)
+    }
+}
