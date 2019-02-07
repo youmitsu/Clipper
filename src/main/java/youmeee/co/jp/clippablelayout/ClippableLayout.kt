68568635fd60632f9adff056218744151e3cf423
YuMitsuhori <ymbullseye@gmail.com>
2019-02-07 21:02:00 +0900

    changed interface of ClippableLayout

--- src/main/java/youmeee/co/jp/clippablelayout/ClippableLayout.kt
+++ src/main/java/youmeee/co/jp/clippablelayout/ClippableLayout.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.FrameLayout
 
 /**
@@ -15,14 +16,25 @@ class ClippableLayout @JvmOverloads constructor(
     defStyle: Int = 0
 ) : FrameLayout(context, attributeSet, defStyle) {
 
+    constructor(
+        context: Context,
+        attributeSet: AttributeSet? = null,
+        defStyle: Int = 0,
+        window: Window
+    ) : this(context, attributeSet, defStyle) {
+        this.window = window
+    }
+
+    private lateinit var window: Window
     var clippableView: ClippableView? = null
     var descView: View? = null
+    var backGroundColor: Int = R.color.default_gray
 
     fun showOverlay(parent: ViewGroup) {
         setOnClickListener {
             removeAllViews()
         }
-        clippableView?.showOverlay(this)
+        clippableView?.showOverlay(this, window, backGroundColor)
         descView?.let { addView(it) }
         parent.addView(this, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT))
         invalidate()
--- src/main/java/youmeee/co/jp/clippablelayout/ClippableView.kt
+++ src/main/java/youmeee/co/jp/clippablelayout/ClippableView.kt
@@ -19,7 +19,7 @@ class ClippableView @JvmOverloads constructor(
 
     private var clipList: MutableList<ClipEntry> = mutableListOf()
     private val decorRect = Rect()
-    var window: Window? = null
+    private var window: Window? = null
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -69,7 +69,9 @@ class ClippableView @JvmOverloads constructor(
 
     fun clear() = clipList.clear()
 
-    fun showOverlay(parent: ViewGroup) {
+    fun showOverlay(parent: ViewGroup, _window: Window, backGroundColorResId: Int) {
+        this.window = _window
+        this.setBackGroundColor(backGroundColorResId)
         parent.removeView(this)
         parent.addView(this)
     }
