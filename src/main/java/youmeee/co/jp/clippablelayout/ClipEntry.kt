75c132128cc39bdce6d085fcbf32ed0b624482c6
YuMitsuhori <ymbullseye@gmail.com>
2019-03-02 20:37:48 +0900

    Added ClipEntry's javadoc

--- src/main/java/youmeee/co/jp/clippablelayout/ClipEntry.kt
+++ src/main/java/youmeee/co/jp/clippablelayout/ClipEntry.kt
@@ -5,6 +5,12 @@ import android.graphics.Paint
 import android.graphics.Rect
 import androidx.annotation.IdRes
 
+/**
+ * ClipEntry
+ * The class expresses the object which is clipped.
+ *
+ * @param resId resources id of the target which is clipped.
+ */
 abstract class ClipEntry(@IdRes val resId: Int) {
 
     constructor() : this(0)
