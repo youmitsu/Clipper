02b500219d43035f07577ef7a700acbe6d4279d2
YuMitsuhori <ymbullseye@gmail.com>
2019-03-03 19:57:12 +0900

    added layoutParams

--- src/main/java/youmeee/co/jp/clippablelayout/ClippableLayout.kt
+++ src/main/java/youmeee/co/jp/clippablelayout/ClippableLayout.kt
@@ -2,6 +2,7 @@ package youmeee.co.jp.clippablelayout
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.Gravity.CENTER_HORIZONTAL
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
@@ -31,12 +32,15 @@ class ClippableLayout : FrameLayout {
     var queueDispatcher: ClippableQueueDispatcher? = null
 
     fun showOverlay(w: Window, p: ViewGroup) {
+        val lp = FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        lp.topMargin = context.resources.getDimensionPixelSize(R.dimen.top_clip_margin)
+        lp.gravity = CENTER_HORIZONTAL
         setOnClickListener {
             (parent as? ViewGroup)?.removeView(this)
             queueDispatcher?.onDetachedClippableView()
         }
         clippableView?.showOverlay(this, w, backGroundColor)
-        descView?.let { addView(it) }
+        descView?.let { addView(it, lp) }
         p.addView(this, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT))
         invalidate()
     }
